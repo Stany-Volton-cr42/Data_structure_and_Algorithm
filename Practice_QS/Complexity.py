e02458101cbf12e4d5b8fe52c1d12e3e4a9e48e0
Vivek raj <stdnow9955@gmail.com>
2024-07-08 02:02:45 +0530

    learn complexity

---
+++ Practice_QS/Complexity.py
@@ -0,0 +1,12 @@
+# Tell me the complexity of the algorithm
+
+n = int(input("Enter the number"))
+
+i = 1
+while i <= n:
+    print("hi")
+    i= i * 2
+
+"""The complexity of the algorithm is O(log n).
+    you can make as the thumb rule that if there is the
+    multiplication and devision then the complexity is log n"""
